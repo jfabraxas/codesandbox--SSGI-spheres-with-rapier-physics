9c665ddcd865a15af6b204ea1544087ab0895d58
幽山 <6962524+jfabraxas@users.noreply.github.com>
2025-08-07 18:43:30 +0200

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 'use client'
+import * as React from 'react'
 import dynamic from 'next/dynamic';
 
 const App = dynamic(() => import('../components/canvas/App').then((mod) => mod.App), {
   ssr: false,
 });
 
-export default function HomePage() {
-  return <App />
+interface HomePageProps {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+
+export default function HomePage({ searchParams }: HomePageProps) {
+  return <React.Suspense><App /></React.Suspense>
 }
