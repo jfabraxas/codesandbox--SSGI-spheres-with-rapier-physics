541d620572727abf219a20155c250bbfdd93e79d
幽山 <6962524+jfabraxas@users.noreply.github.com>
2025-08-07 18:54:41 +0200

    Extract all shader constants from v2.ts

---
+++ src/components/canvas/realism-effects/shaders/traaComposeShader.ts
@@ -0,0 +1,1 @@
+export const traaComposeShader = "#define GLSLIFY 1\nuniform sampler2D accumulatedTexture;void mainImage(const in vec4 inputColor,const in vec2 uv,out vec4 outputColor){vec4 accumulatedTexel=textureLod(accumulatedTexture,uv,0.);outputColor=vec4(accumulatedTexel.rgb,1.);}"; // eslint-disable-line
---
+++ src/components/canvas/realism-effects/shaders/vertexShader.ts
@@ -0,0 +1,1 @@
+export const vertexShader = "#define GLSLIFY 1\nvarying vec2 vUv;void main(){vUv=position.xy*0.5+0.5;gl_Position=vec4(position.xy,1.0,1.0);}"; // eslint-disable-line
